6fce2b1d6bbc84c7375963657177e7dc97917f93
kslai <43594906+nagomikslai@users.noreply.github.com>
2022-11-02 19:45:25 +0800

    commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  network_interface {
+    network_interface_id = var.network_interface_id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
